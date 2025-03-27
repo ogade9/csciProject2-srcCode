a6fe021b320f7b8120e0ece4098efbbdc4c5b2a6
ogade9 <112106645+ogade9@users.noreply.github.com>
2025-03-27 11:10:10 -0400

    Deleted unnessessary files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+
 </script>
 
 <template>
-    <RouterView />
+  <main>
+    Hello World!
+  </main>
 </template>
-
 <style scoped>
 </style>
